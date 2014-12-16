ae19d61e61497400b98ea1aa906d3d03c2eab951
Stephen Seliuk <stephen@seliuk.com>
2014-12-16 21:59:21 +0200

    Bug fixes

--- create-swapfile.sh
+++ create-swapfile.sh
@@ -2,8 +2,10 @@
 
 # https://www.digitalocean.com/community/tutorials/how-to-add-swap-on-centos-6
 # create swapfile
-dd if=/dev/zero of=/swapfile bs=1024 count=1024k
+dd if=/dev/zero of=/swapfile bs=1024 count=512k
 mkswap /swapfile
 swapon /swapfile
 chown root:root /swapfile
 chmod 0600 /swapfile
+
+# NOT WORKED on OpenVZ virtualization
