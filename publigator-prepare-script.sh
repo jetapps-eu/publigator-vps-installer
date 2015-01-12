cdc79c86778de450aec1d74be1781fcbddef131d
Stephen Seliuk <stephen@seliuk.com>
2015-01-13 01:44:12 +0200

    0.2.13-beta

--- publigator-prepare-script.sh
+++ publigator-prepare-script.sh
@@ -7,7 +7,7 @@ source "$CONFIGPATH/publigator-config.sh"
 cd $INSTALLDIR
 # ===========================================
 
-pblfile='publigator-0.2.12-beta.zip'
+pblfile='publigator-0.2.13-beta.zip'
 
 # next need to download Publigator (latest) to /home/admin/web/publigator
 wget -N "http://dist.publigator.com/$pblfile"
